64fc785a3cde9a5db92cf667b23c92d13e1d0119
Thomas Aylott <oblivious+git@subtlegradient.com>
2011-12-31 23:58:08 -0800

    NEW support for XML as a string in the parser

--- parse.js
+++ parse.js
@@ -8,26 +8,19 @@ var sax = require('sax')
 
 exports.parse =
 exports.decode =
-
-function(path, callback){
+exports.parseFile =
+exports.decodeFile =
+exports.parseStream =
+exports.decodeStream =
+function(xml, callback){
     var DATA = {}
-    // stream usage
-    // takes the same options as the parser
-    var saxStream = sax.createStream(false, {
-        trim: true // Boolean. Whether or not to trim text and comment nodes.
-        ,
-        normalize: true // Boolean. If true, then turn any whitespace into a single space.
-        ,
-        lowercasetags: true // Boolean. If true, then lowercase tags in loose mode, rather than uppercasing them.
-        ,
-        xmlns: false // Boolean. If true, then namespaces are supported.
-    })
 
-    saxStream.on('error', function(error) {
+    function error(error) {
         callback(error)
         this._parser.error = null
         this._parser.resume()
-    })
+    }
+    
 
     var tagHandlers = {}
 
@@ -64,43 +57,81 @@ function(path, callback){
         delete node.parentNode
     }
 
-    saxStream.on('opentag', function(node){
+    function opentag(node){
         if (tagHandlers[node.name]) tagHandlers[node.name](node)
         else tagHandlers._default(node)
-    })
-
-    saxStream.on('closetag', function(nodeName) {
+    }
+    function closetag(nodeName) {
         if (tagHandlers[nodeName] && tagHandlers[nodeName].close)
             tagHandlers[nodeName].close(nodeName)
         else tagHandlers._default.close(nodeName)
-    })
-
-    saxStream.on('text', setValue)
-
+    }
     function setValue(value) {
-        if (Object.keys(DATA).length == 1) DATA.toJSON = DATA.valueOf = returnThisValue
-    
-        if (!DATA.value) DATA.value = value
+        if (!DATA.text) DATA.text = value
         else {
-            if (!Array.isArray(DATA.value)) DATA.value = [DATA.value]
-            DATA.value.push(value)
+            if (!Array.isArray(DATA.text)) DATA.text = [DATA.text]
+            DATA.text.push(value)
         }
     }
 
     var CDATA = ''
-    saxStream.on('cdata', function(cdataChunk) {
+    function cdata(cdataChunk) {
         CDATA += cdataChunk
-    })
-    saxStream.on('closecdata', function() {
+    }
+    
+    function closecdata() {
         setValue(CDATA)
         CDATA = ''
-    })
-    saxStream.on('end', function() {
+    }
+    
+    function end() {
         callback(null, DATA)
         DATA = {}
-    })
+    }
 
-    fs.createReadStream(path).pipe(saxStream)
+    ////////////////////////////////////////////////////////////////////////////////
+
+    var options = {
+        strict: false
+        ,
+        trim: true // Boolean. Whether or not to trim text and comment nodes.
+        ,
+        normalize: true // Boolean. If true, then turn any whitespace into a single space.
+        ,
+        lowercasetags: true // Boolean. If true, then lowercase tags in loose mode, rather than uppercasing them.
+        ,
+        xmlns: false // Boolean. If true, then namespaces are supported.
+    }
+
+    if (xml.pipe) xml.pipe(saxStream)
+    else if (require('path').existsSync(xml))
+        xml = fs.createReadStream(xml)
+
+    if (xml.pipe) {
+        var saxStream = sax.createStream(options.strict, options)
+        saxStream.on('error', error)
+        saxStream.on('opentag', opentag)
+        saxStream.on('closetag', closetag)
+        saxStream.on('text', setValue)
+        saxStream.on('cdata', cdata)
+        saxStream.on('closecdata', closecdata)
+        saxStream.on('end', end)
+        
+        xml.pipe(saxStream)
+    }
+    else {
+        var saxParser = sax.parser(options.strict, options)
+        saxParser.onerror = error
+        saxParser.onopentag = opentag
+        saxParser.onclosetag = closetag
+        saxParser.ontext = setValue
+        saxParser.oncdata = cdata
+        saxParser.onclosecdata = closecdata
+        saxParser.onend = end
+        
+        saxParser.write('' + xml)
+        saxParser.close()
+    }
 }
 
 function returnThisValue(){return this.value}
@@ -108,7 +139,8 @@ function returnThisValue(){return this.value}
 ////////////////////////////////////////////////////////////////////////////////
 
 if (module.id == '.')new function(){
-    
+    var paths
+    =
     [    __dirname + "/test/TEST-net.cars.documents.AssuranceTest.xml"
     ,    __dirname + "/test/TEST-net.cars.engine.BougieTest.xml"
     ,    __dirname + "/test/TEST-net.cars.engine.CarburateurTest.xml"
@@ -119,18 +151,24 @@ if (module.id == '.')new function(){
     ,    __dirname + "/test/TEST-net.cars.engine.PistonTest.xml"
     ]
     
-    .forEach(function(path){
-        exports.decode(path, function(error, report){
-            if (error) return console.error(error)
-            // console.log(report)
-            console.log('[START]\t' + report.testsuite.name)
-            ;[].concat(report.testsuite.testcase).map(function(testcase){
-                console.log(
-                    '['+(testcase.failure&&'FAIL' || testcase.error&&'ERROR' || "PASS")+']\t'
-                        + testcase.classname + '#' + testcase.name
-                )
-            })
-            console.log('')
-        })
+    paths.forEach(function(path){
+        exports.decodeFile(path, log)
     })
+    
+    paths.forEach(function(path){
+        exports.decode(fs.readFileSync(path), log)
+    })
+    
+    function log(error, report){
+        if (error) return console.error(error)
+        // console.log(report)
+        console.log('[START]\t' + report.testsuite.name)
+        ;[].concat(report.testsuite.testcase).map(function(testcase){
+            console.log(
+                '['+(testcase.failure&&'FAIL' || testcase.error&&'ERROR' || "PASS")+']\t'
+                    + testcase.classname + '#' + testcase.name
+            )
+        })
+        console.log('')
+    }
 }
